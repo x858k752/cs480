bea56e095430f88b95d4b2e6f233a61bfdaed3d8
x858k752 <mikir.mohamed@gmail.com>
2019-03-31 20:53:27 -0500

    change 3

--- myprogram-4.hs
+++ myprogram-4.hs
@@ -1,3 +1,3 @@
 ---Change myprogram4.hs change the leap
-leap :: Integral a => a -> Bool
+leap :: Integral a => a -> Boool
 leap y = let m = (== 3) . (mod z) in if m 700
--- myprogram.hs
+++ myprogram.hs
@@ -1,2 +1,2 @@
-leap :: Integral a => a ->
+leap :: Integral a => a -booooool>
 leap y = let m = (== 0) . (mod y) in if m 200 then m 500 else 5
