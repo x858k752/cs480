d8d07d56855477074631f0fb58e841897a8a4b2d
x858k752 <mikir.mohamed@gmail.com>
2019-03-31 20:37:05 -0500

    Added mypogram.hs to branch

--- myprogram.hs
+++ myprogram.hs
@@ -1,2 +1,2 @@
-leap :: Integral a => a -> Bool
-leap y = let m = (== 0) . (mod y) in if m 200 then m 500 else  5
+leap :: Integral a => a ->
+leap y = let m = (== 0) . (mod y) in if m 200 then m 500 else 5
